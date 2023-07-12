59800267ce91587bf776cd13a7e18f3a90bbb7f8
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-07-12 14:26:02 -0300

    Update validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -18,7 +18,7 @@ function verificaSeOChutePossuiUmValorValido(chute) {
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
 
-            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
         `
     } else if (numero > numeroSecreto) {
         elementoChute.innerHTML += `
@@ -29,7 +29,6 @@ function verificaSeOChutePossuiUmValorValido(chute) {
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
     }
- 
 }
 
 function chuteForInvalido(numero) {
@@ -41,7 +40,7 @@ function numeroForMaiorOuMenorQueOValorPermitido(numero){
 }
 
 document.body.addEventListener('click', e => {
-    if(e.target.id == 'jogar-novamente'){
-    window.location.reload()
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
     }
 })
